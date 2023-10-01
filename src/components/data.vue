bc05daf0fc478b6e5099b14e0d6b92fb6610af2a
fredrikofstad <nokotin@gmail.com>
2023-10-01 21:10:07 +0200

    added d3

--- src/components/data.vue
+++ src/components/data.vue
@@ -1,13 +1,38 @@
 <script>
+import * as d3 from "d3";
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    const width = 600;
+    const height = 300;
+    const popMax = 1_380_000_000
+    const popMin = 52_070
 
+    const svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+    const y = d3.scaleLinear()
+        .domain([15, 90]).range([250, 0])
+    const x = d3.scaleLog()
+        .domain([250, 100_000]).range([0,width])
+
+    const radius = d3.scaleSqrt()
+        .domain([popMin, popMax]).range([10,40])
+    svg.append('circle')
+        .attr('fill', 'red')
+        .attr('r', radius(popMax))
+        .attr('cx', x(13330))
+        .attr('cy', y(77))
+  },
+};
 </script>
 
 <template>
-  <h2>Data Visualization with d3.js</h2>
-  <svg></svg>
-
+  <div>
+    <h2>Data visualization with D3.js</h2>
+    <svg></svg>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
